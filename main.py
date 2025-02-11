f6c8570369b7d19d8bb1b50d558e7da97f66837a
IDGS-801-22002343 <80266@alumnos.utleon.edu.mx>
2025-02-10 18:41:17 -0600

    Etiqueta: Container 10-02-2025

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    titulo = "IDGS801"
+    lista = ["Juan", "Carlos", "Oscar"]
+    return render_template("index.html", titulo = titulo, lista = lista)
+
+@app.route("/ejemplo1")
+def ejemplo1():
+    return render_template("ejemplo1.html")
+
+@app.route("/ejemplo2")
+def ejemplo2():
+    return render_template("ejemplo2.html")
 
 
 @app.route("/hola")
@@ -45,7 +55,7 @@ def operas():
 
         <label for="name">apaterno:</label>
         <input type="text" id="name" name="name" required>
-    </form>
+               </form>
             '''
 
 
